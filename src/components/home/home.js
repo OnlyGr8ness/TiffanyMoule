75ecce35528612aaf14a33756c233d6af50c3b13
OnlyGr8ness <Sgt.moule@gmail.com>
2018-03-22 23:42:24 -0700

    sticky nav scrollbar hidden

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Home extends Component {
   render() {
     return (
-      <div className="bodyContent">
+      <div>
 
         <div>
           <div className="container-fluid hero-container">
